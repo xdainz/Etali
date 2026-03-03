471c35fc0922080366ac38f7173454f6126ebd1f
dainz <mbenjaminarancibia@gmail.com>
2026-03-03 17:24:46 -0300

    set curl staticly

--- fetch/fetch.cpp
+++ fetch/fetch.cpp
@@ -10,34 +10,38 @@ static size_t write_callback(void *contents, size_t size, size_t nmemb, void *us
     return real_size;
 }
 
-std::string fetch_random_commander(std::string args){
-
-    const std::string API_URL = BASE_URL + "random?q=is%3Acommander";
-
-    CURL *curl = curl_easy_init();
-    std::string response_body;
-
-    if(!curl){
-        return std::string("ERROR:1");
-    }
-
+static CURL* set_curl(){
+    CURL* curl = curl_easy_init();
     // set headers
     struct curl_slist *headers = NULL;
     headers = curl_slist_append(headers, "Accept: */*");
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
     curl_easy_setopt(curl, CURLOPT_USERAGENT, USER_AGENT.c_str());
 
+    // set write callback to capture response body
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+
+    return curl;
+}
     // set url (use c_str())
     curl_easy_setopt(curl, CURLOPT_URL, API_URL.c_str());
 
+std::string fetch_random_commander(std::string args){
+    const std::string API_URL = BASE_URL + "random?q=is%3Acommander";
+    std::string response_body;
+    CURL* curl = set_curl();
+
+    if(!curl){
+        return std::string("ERROR:1");
+    }
+    // set url (use c_str())
+    curl_easy_setopt(curl, CURLOPT_URL, API_URL.c_str());
     // set write callback to capture response body
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_body);
 
     CURLcode res = curl_easy_perform(curl);
 
     // cleanup
-    if(headers) curl_slist_free_all(headers);
     curl_easy_cleanup(curl);
 
     if(res != CURLE_OK){
